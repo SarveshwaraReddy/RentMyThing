054f4693b413cf3245fb2050bd03ddead36d9ec5
SarveshwaraReddy <sarveshwarareddy9@gmail.com>
2026-06-03 12:48:52 +0530

    style: remove custom cursor and restore default cursor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { AnimatePresence } from "framer-motion";
 import AppRoutes from "./routes/AppRoutes.jsx";
 import Navbar from "./components/Navbar.jsx";
-import CustomCursor from "./components/CustomCursor.jsx";
 
 function App() {
   return (
     <div className="app-shell">
-      <CustomCursor />
       <Navbar />
       <AnimatePresence mode="wait">
         <AppRoutes />
